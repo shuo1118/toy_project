5e348e3b3997fd94b20153e02b2b572128397f3e
shuo1118 <125795495+shuo1118@users.noreply.github.com>
2023-06-26 15:29:00 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="my_package",
-    version="0.3",
+    version="0.1",
     packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'my_package=my_package.main:main',
+        ],
+    },
 )
+
